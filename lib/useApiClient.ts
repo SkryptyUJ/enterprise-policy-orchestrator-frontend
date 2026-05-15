1759521a6a62ca90b6e88498638280e7056f5329
mkosciolek <michal.kosc20@gmail.com>
2026-05-15 12:33:58 +0200

    containerize application
    # Conflicts:
    #	app/api/auth/[auth0]/route.ts

--- lib/useApiClient.ts
+++ lib/useApiClient.ts
@@ -1,15 +1,13 @@
 "use client"
 
+import { getAccessToken } from "@auth0/nextjs-auth0/client"
 import { useMemo } from "react"
 import { createApiClient } from "./apiClient"
 
 export function useApiClient() {
     return useMemo(() => {
         return createApiClient(async () => {
-            // TODO: zastąp po instalacji @auth0/nextjs-auth0:
-            // const { getAccessTokenSilently } = useAuth0()
-            // return getAccessTokenSilently()
-            return "placeholder-token"
+            return getAccessToken()
         })
     }, [])
 }
